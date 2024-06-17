dd25861dfa1732d6897b016929f1a54cb9a1c483
jessicalexander <ssicalexander@gmail.com>
2024-06-18 01:18:08 +0700

    tambahan fitur

--- public/pages/js/plp.js
+++ public/pages/js/plp.js
@@ -5,6 +5,7 @@ function getDataOnEnter(event){
         getData(1);
     }
 }
+
 function getData(toPage=1){
     let url = baseUrl+'/api/book';
     if(toPage){
@@ -15,100 +16,111 @@ function getData(toPage=1){
         '_page': toPage
     };
     $("._filter").each(function() {
-      payload[$(this).attr('name')] = $(this).val();
+        payload[$(this).attr('name')] = $(this).val();
     });
     
-    axios.get(url,{params:payload},apiHeaders)
+    axios.get(url, { params: payload }, apiHeaders)
     .then(function (response) {
-        console.log('[DATA] response..',response.data);
+        console.log('[DATA] response..', response.data);
         let template = ``;
         // START-- products
-            (response.data.products).forEach((item) => {
-                template += `   <div class="col-lg-3 col-md-4 col-sm-6">
-                                    <div class="single-product-item text-center">
-                                        <div class="products-images">
-                                            <a href="/book/`+item.id+`" class="product-thumbnail">
-                                                <img src="`+item.cover+`" alt="Product Images" height="300">
-                                            </a>
-                                            <div class="product-actions">
-                                                <a href="/book/`+item.id+`"><i class="p-icon icon-plus"></i><span class="tool-tip">Quick View</span></a>
-                                                <a href="#"><i class="p-icon icon-bag2"></i> <span class="tool-tip">Add to cart</span></a>
-                                            </div>
-                                        </div>
-                                        <div class="product-content">
-                                            <h6 class="product-title">
-                                                <a href="/book/`+item.id+`">`+item.title+`</a>
-                                            </h6>
-                                            <small class="text-color-primary">`+item.author+`</small>
-                                            <div class="product-price">
-                                                <span class="new-price">IDR `+parseFloat(item.price).toLocaleString()+`</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
-            });
-            $(id_el_list).html(template);
+        (response.data.products).forEach((item) => {
+            template += `   
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="single-product-item text-center">
+                        <div class="products-images">
+                            <a href="/book/${item.id}" class="product-thumbnail">
+                                <img src="${item.cover}" alt="Product Images" height="300">
+                            </a>
+                            <div class="product-actions">
+                                <a href="/book/${item.id}"><i class="p-icon icon-plus"></i><span class="tool-tip">Quick View</span></a>
+                                <a href="#"><i class="p-icon icon-bag2"></i> <span class="tool-tip">Add to cart</span></a>
+                            </div>
+                        </div>
+                        <div class="product-content">
+                            <h6 class="product-title">
+                                <a href="/book/${item.id}">${item.title}</a>
+                            </h6>
+                            <small class="text-color-primary">${item.author}</small>
+                            <div class="product-price">
+                                <span class="new-price">IDR ${parseFloat(item.price).toLocaleString()}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+        });
+        $(id_el_list).html(template);
         // END---- products
         // START-- pagination
-            $('#products_count_start').html(response.data.products_count_start);
-            $('#products_count_end').html(response.data.products_count_end);
-            $('#products_count_total').html(response.data.products_count_total);
-            template = '';
-            let max_page = Math.ceil(response.data.products_count_total/response.data.filter._limit);
-            if(response.data.filter._page != 1){
-                template += 
-                `<li>
-                    <a class="prev page-numbers" onclick="getData(1)">
-                        <i class="icon-chevron-left"></i>&nbsp;&nbsp;&nbsp;Min Page
-                    </a>
-                </li>`; 
-            }
-            if(response.data.filter._page > 1){
-                template += 
-                `<li>
-                    <a class="page-numbers" onclick="getData(`+(response.data.filter._page-1)+`)">
-                        `+(response.data.filter._page-1)+`
-                    </a>
-                </li>`; 
-            }
+        $('#products_count_start').html(response.data.products_count_start);
+        $('#products_count_end').html(response.data.products_count_end);
+        $('#products_count_total').html(response.data.products_count_total);
+        template = '';
+        let max_page = Math.ceil(response.data.products_count_total / response.data.filter._limit);
+        if (response.data.filter._page != 1) {
             template += 
-                `<li>
-                    <a class="current text-white page-numbers" onclick="getData(`+response.data.filter._page+`)">
-                        `+response.data.filter._page+`
-                    </a>
-                </li>`; 
-            if(response.data.filter._page < max_page){
-                template += 
-                `<li>
-                    <a class="page-numbers" onclick="getData(`+(response.data.filter._page+1)+`)">
-                        `+(response.data.filter._page+1)+`
-                    </a>
-                </li>`; 
+            `<li>
+                <a class="prev page-numbers" onclick="getData(1)">
+                    <i class="icon-chevron-left"></i>&nbsp;&nbsp;&nbsp;Min Page
+                </a>
+            </li>`; 
+        }
+        if (response.data.filter._page > 1) {
+            template += 
+            `<li>
+                <a class="page-numbers" onclick="getData(${response.data.filter._page - 1})">
+                    ${response.data.filter._page - 1}
+                </a>
+            </li>`; 
+        }
+        template += 
+            `<li>
+                <a class="current text-white page-numbers" onclick="getData(${response.data.filter._page})">
+                    ${response.data.filter._page}
+                </a>
+            </li>`; 
+        if (response.data.filter._page < max_page) {
+            template += 
+            `<li>
+                <a class="page-numbers" onclick="getData(${response.data.filter._page + 1})">
+                    ${response.data.filter._page + 1}
+                </a>
+            </li>`; 
+        }
+        if (response.data.filter._page + 1 < max_page) {
+            template += 
+            `<li>
+                <a class="page-numbers" onclick="getData(${response.data.filter._page + 2})">
+                    ${response.data.filter._page + 2}
+                </a>
+            </li>`;
+        }
+        if (response.data.filter._page < max_page) {
+            template += 
+            `<li>
+                <a class="next page-numbers" onclick="getData(${max_page})">
+                    Max Page<i class="icon-chevron-right"></i>
+                </a>
+            </li>`; 
+        }
+        if (max_page > 1) {
+            template += 
+            `<li>
+                <select class="page-numbers" id="jumpPageNumber" onchange="jumpToPage(this.value)">
+                    <option value="" disabled>Jump to page</option>`;
+            for (let i = 1; i <= max_page; i++) {
+                template += `<option value="${i}" ${i === response.data.filter._page ? 'selected' : ''}>${i}</option>`;
             }
-            if(response.data.filter._page+1 < max_page){
-                template += 
-                `<li>
-                    <a class="page-numbers" onclick="getData(`+(response.data.filter._page+2)+`)">
-                        `+(response.data.filter._page+2)+`
-                    </a>
-                </li>`;
-            }
-            if(response.data.filter._page < max_page){
-                template += 
-                `<li>
-                    <a class="next page-numbers" onclick="getData(`+max_page+`)">
-                        Max Page<i class="icon-chevron-right"></i>
-                    </a>
-                </li>`; 
-            }
-            $(id_el_list+'-pagination').html(template);
-            $('[name="_page"]').val(response.data.filter._page);
+            template += `</select>
+            </li>`;
+        }        
+        $(id_el_list + '-pagination').html(template);
+        $('[name="_page"]').val(response.data.filter._page);
         // END---- pagination
-
     })
     .catch(function (error) {
-        console.log('[ERROR] response..',error);
-        if(error.code == "ERR_BAD_REQUEST"){
+        console.log('[ERROR] response..', error);
+        if (error.code == "ERR_BAD_REQUEST") {
             Swal.fire({
                 position: "top-end",
                 icon: "warning",
@@ -117,7 +129,7 @@ function getData(toPage=1){
                 showConfirmButton: false,
                 timer: 5000
             });
-        }else{
+        } else {
             Swal.fire({
                 icon: 'error',
                 width: 600,
@@ -127,6 +139,11 @@ function getData(toPage=1){
             });
         }
     });
+}
+
+// Function to handle the jump to page event
+function jumpToPage(page) {
+    getData(page);
 }
 
 $(function () {
